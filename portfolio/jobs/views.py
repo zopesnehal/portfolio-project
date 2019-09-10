f42c5cbbe79ae157d6bfc2ed167ae40a7d3848c6
Nikhil Rane <nikhilrane1992@gmail.com>
2019-09-10 10:39:56 +0530

    added job file new

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -3,5 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from models import Job
+
 def home(request):
-	return render(request, 'jobs/home.html')
+
+	jobs = Job.objects
+
+	return render(request, 'jobs/home.html', {'jobs':jobs})
